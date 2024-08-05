d2e1a7d23ba565f0772204e92336462cb568785b
Jurabek <jorabeksindarov44@gmail.com>
2024-08-05 22:50:31 +0500

    work with continue statement

--- Index.php
+++ Index.php
@@ -10,3 +10,18 @@ do {
    }
    $value -= 1;
 }while($value > 0);*/
+
+
+// Work with continue
+
+/*for ($i = 0; $i < 10; $i++) {
+    if ($i % 2 === 0) {
+        continue;
+    }
+    elseif ($i == 7){
+        continue;
+    }
+    echo "$i\n";
+}
+result : 1,3,5,9
+*/
