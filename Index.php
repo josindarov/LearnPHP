3d23d97365e1d99c4f924fcc54654dc334955599
Jurabek <jorabeksindarov44@gmail.com>
2024-08-05 22:47:12 +0500

    Initial commit

---
+++ Index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Work with do while statement
+
+/*$value = readline("Enter number: ");
+
+do {
+   if($value % 2 == 0) {
+       echo $value . "\n";
+   }
+   $value -= 1;
+}while($value > 0);*/
