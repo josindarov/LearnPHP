3a1a964768826789e5ea87703ee41cba4a98dedf
Jurabek <jorabeksindarov44@gmail.com>
2024-08-06 14:41:44 +0500

    docker compose added

--- Index.php
+++ Index.php
@@ -28,18 +28,30 @@ result : 1,3,5,9
 
 // work with functions
 
-function Add($a, $b){
-    return $a + $b;
-}
+//function Add($a, $b){
+//    return $a + $b;
+//}
+//
+//function Sub($a, $b){
+//    return $a - $b;
+//}
+//
+//function Length($word){
+//    return strlen($word);
+//}
+//
+//Print(Add(1,200));
+//Print(Sub(1,200));
+//Print(Length(Add(1,200)));
 
-function Sub($a, $b){
-    return $a - $b;
-}
 
-function Length($word){
-    return strlen($word);
-}
+// function get_counter()
+// {
+//     static $counter = 1;
+//     return $counter++;
+// }
 
-Print(Add(1,200));
-Print(Sub(1,200));
-Print(Length(Add(1,200)));
+// echo get_counter() . "\n";
+// echo get_counter() . "\n";
+// echo get_counter() . "\n";
+?>
